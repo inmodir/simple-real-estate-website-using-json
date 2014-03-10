1d1bb4b102e36c95361811060745355b488e732f
Christian Valentini <christianvalentini@gmail.com>
2014-03-10 13:01:30 -0200

    Update ads_content_viewlist.php

--- includes/sections/ads_content_viewlist.php
+++ includes/sections/ads_content_viewlist.php
@@ -5,19 +5,25 @@
 echo"  <div id=\"content\">\n";
 echo"   <div class=\"wrap\">\n";
 echo"    <h2>".$sectionTitle."</h2>\n";
-echo"    <ul class=\"adslist\">\n";
-foreach($inmodir_array['optdata'] as $myads)
-{ 
- echo"     <li><a href=\"?acode=".$myads['ad_acode']."\">";//to 'forrent' section
- echo"<p class=\"txt\">".$myads['ad_introduction']." <em>".$myads['ad_price']."&euro;</em></p>";
- echo"<p class=\"img\"><img src=\"";
- if($myads['ad_defaultpic']!="0"){echo$myads['ad_defaultpic'];}//default photo
- else{echo$websiteUrlStatic."/img/nophoto.png";}//case ad has no photo (zero between quotes!!)
- echo"\" alt=\"".$myads['ad_introduction']."\" /></p>";
- echo"</a></li>\n";
+if($inmodir_array['optnumber']<1)//No ads for this type (all be expired?)
+{
+ echo"    <p>There are no listings available at this time.</p>\n";
 }
-echo"    </ul>\n";
+ else
+{
+ echo"    <ul class=\"adslist\">\n";
+ foreach($inmodir_array['optdata'] as $myads)
+ { 
+  echo"     <li><a href=\"?acode=".$myads['ad_acode']."\">";//to 'forrent' section
+  echo"<p class=\"txt\">".$myads['ad_introduction']." <em>".$myads['ad_price']."&euro;</em></p>";
+  echo"<p class=\"img\"><img src=\"";
+  if($myads['ad_defaultpic']!="0"){echo$myads['ad_defaultpic'];}//default photo
+  else{echo$websiteUrlStatic."/img/nophoto.png";}//case ad has no photo (zero between quotes!!)
+  echo"\" alt=\"".$myads['ad_introduction']."\" /></p>";
+  echo"</a></li>\n";
+ }//foreach
+ echo"    </ul>\n";
+}//case
 echo"   </div>\n";//wrap
 echo"  </div>\n";//content
-// var 'adsinfo_errorName' defined at 'includes/sections/ads.php'
 ?>
